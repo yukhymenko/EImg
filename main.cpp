d7c225a420c9b34360dfc84d9e4e6595e5623169
Oleksii Yukhymenko <al.yukhymenko@gmail.com>
2018-03-14 22:25:47 +0200

    Init commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include "cfile.h"
 
 
-int main() {
-    EImg::CFile f1("q1.bmp");
 
-    std::cout  << "f1.name() " << f1.getFName() << std::endl;
-    std::cout << "f1.size() " << f1.getFSize() << std::endl;
-    f1.save("w1.bmp");
+int main(int argc, char *argv[]) {
 
     return 0;
 }
